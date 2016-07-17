839dce45fc0e736e9dff6cc859363bb128605d7c
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2016-07-17 18:42:41 +0200

    Fully working communication between checkpoints, mobile, server and client, maps, followings

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,42 @@
-/**
- * GOOGLE MAPS
- */
-
-var map;
-var markers = {};
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 50.7861757, lng: 16.1513443},
-        zoom: 8
-    });
-
-    var ctaLayer = new google.maps.KmlLayer({
-        url: '../trasa.kml',
-        map: map
-    });
-};
-
 $(document).ready(function () {
+
+    var map;
+    var markers = {};
+
+    map = L.map('map');
+
+    var service = new L.tileLayer('https://api.mapbox.com/styles/v1/krisu/ciqpgxyy0003wcankixy95bud/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia3Jpc3UiLCJhIjoiY2lxcGd5enllMDA2MGh5bWFrYnA0Z2hwMiJ9.4j9N70VORhhTh1eUOSjL3A', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18
+    }).addTo(map);
+
+    var el = L.control.elevation();
+    el.addTo(map);
+
+    var gpx = 'trasa.gpx'; // URL to your GPX file or the GPX itself
+    var g = new L.GPX(gpx, {
+        async: true,
+        marker_options: {
+            startIconUrl: '../images/pin-icon-start.png',
+            endIconUrl: '../images/pin-icon-end.png',
+            shadowUrl: '../images/pin-shadow.png'
+        }
+    });
+    g.on('loaded', function(e) {
+        map.fitBounds(e.target.getBounds());
+    });
+    g.on("addline",function(e){
+        el.addData(e.line);
+    });
+    g.addTo(map);
+    map.addLayer(service);
 
     /**
      * SOCKETS
      */
 
-    var table = $('#event-data-table').DataTable();
+    var generalTable = $('#event-data-table').DataTable();
+    var followingTable = $('#following-data-table');
 
     var socket = io();
 
@@ -35,48 +48,76 @@ $(document).ready(function () {
     socket.on('refresh', function (data) {
 
         console.log('CHECKPOINT REFRESH');
-        var $cell = $("td[data-checkpoint-id='" + data.checkpoint_id +"'][data-participant-id='" + data.participant_id +"']");
+        var generalTableCell = $('#event-data-table').find("td[data-checkpoint-id='" + data.checkpoint_id +"'][data-participant-id='" + data.participant_id +"']").eq(0);
+        var followingTableCell = $('#following-data-table').find("td[data-checkpoint-id='" + data.checkpoint_id +"'][data-participant-id='" + data.participant_id +"']").eq(0);
 
-        var tableCell = table.cell($cell);
-        tableCell.data(data.time);
-        $cell.addClass('updated');
-        tableCell.draw();
-        setTimeout(function () {
-            $cell.removeClass('updated');
-        }, 3000);
+        // Update time in general table
+        if(generalTableCell.length > 0) {
+            var tableCell = generalTable.cell(generalTableCell);
+            tableCell.data(data.time);
+            generalTableCell.addClass('updated');
+            tableCell.draw();
+            setTimeout(function () {
+                generalTableCell.removeClass('updated');
+            }, 3000);
+        }
 
+        // Update time in following table
+        if(followingTableCell.length > 0) {
+            followingTableCell.text(data.time);
+            followingTableCell.addClass('updated');
+            setTimeout(function () {
+                followingTableCell.removeClass('updated');
+            }, 3000);
+        }
     });
-
 
     socket.on('location-update', function (data) {
         console.log('location-update');
         console.log(data);
 
-        $('#event-id').append(`${data.lat}, ${data.lng}`);
-
         var marker = markers[data.participant_event_id];
         if(marker){
-            if(!marker.getMap()){
-                marker.setMap(map);
+            if(marker.getLatLng().equals(L.latLng(0,0))){
+                marker.addTo(map);
             }
-            marker.setPosition(new google.maps.LatLng(data.lat, data.lng));
+            marker.setLatLng(L.latLng(data.lat, data.lng));
         }
     });
 
-
+    /**
+     * FOLLOW & UNFOLLOW
+     */
     $('#event-data-table')
         .on('click', 'span.follow', function () {
+            // Get participant id, hide button Follow and show button Unfollow
             var id = $(this).parents('tr').data('participant-event-id');
             $(this).hide();
             $(this).siblings('.unfollow').show();
-            console.log(socket.id);
+
+            // Subscribe to location updates of the selected participant
             socket.emit('follow-participant', id);
 
-            var marker = new google.maps.Marker({
-                position: {lat: 0.0, lng: 0.0},
-                participant_id: id
-            });
+            // Get participant's name
+            var participantName = $(this).parent().siblings('.participant_name').text();
+
+            // Create marker on the map
+            var marker = L.marker([0, 0])
+                .bindLabel(`${participantName}`, {
+                    noHide: true,
+                    direction: 'auto'
+                });
             markers[id] = marker;
+
+            // Copy row to Following table
+            var row = $(this).parents('tr').clone();
+            var cells = row.children('td');
+            // Remove Category, Team, City and Location columns
+            cells.splice(2,3);
+            cells.splice(cells.length - 1, 1);
+            followingTable.children('tbody').append('<tr data-participant-event-id="' + id + '">');
+            followingTable.children('tbody').children('tr:last').append(cells);
+
         })
         .on('click', 'span.unfollow', function () {
             var id = $(this).parents('tr').data('participant-event-id');
@@ -84,6 +125,9 @@ $(document).ready(function () {
             $(this).siblings('.follow').show();
             socket.emit('unfollow-participant', id);
             delete markers[id];
+
+            // Remove row from Following table
+            followingTable.find('tr[data-participant-event-id="'+id+'"]').remove();
         });
 
 
