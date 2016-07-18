a395543585131ef8b6e38ae82c60b0ae0ac8993d
Krzysztof Gaszyński <k.gaszynski@gmail.com>
2016-07-18 16:43:55 +0200

    Port change to 3001

--- sockets.js
+++ sockets.js
@@ -22,8 +22,8 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(3001, function(){
+    console.log('listening on *:3001');
 });
 
 /**
